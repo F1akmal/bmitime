e41cadc5f874d8eb56b6a17c2e539fb504a7367a
F1akmal <18DDT23F1103@student.ptss.edu.my>
2025-01-22 16:48:49 +0800

    Coding dah siap

--- bmitime/Form1.Designer.vb
+++ bmitime/Form1.Designer.vb
@@ -22,10 +22,107 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Label1 = New Label()
+        lblBerat = New Label()
+        Label2 = New Label()
+        nudBerat = New NumericUpDown()
+        nudTinggi = New NumericUpDown()
+        lblArahan = New Label()
+        BtnSubmit = New Button()
+        CType(nudBerat, ComponentModel.ISupportInitialize).BeginInit()
+        CType(nudTinggi, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 26.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(281, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(192, 47)
+        Label1.TabIndex = 0
+        Label1.Text = "BMI Kamu"
+        ' 
+        ' lblBerat
+        ' 
+        lblBerat.AutoSize = True
+        lblBerat.Location = New Point(43, 112)
+        lblBerat.Name = "lblBerat"
+        lblBerat.Size = New Size(55, 15)
+        lblBerat.TabIndex = 1
+        lblBerat.Text = "Berat(kg)"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(43, 186)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(79, 15)
+        Label2.TabIndex = 2
+        Label2.Text = "Tinggi(meter)"
+        ' 
+        ' nudBerat
+        ' 
+        nudBerat.DecimalPlaces = 2
+        nudBerat.Location = New Point(198, 114)
+        nudBerat.Maximum = New Decimal(New Integer() {999, 0, 0, 0})
+        nudBerat.Name = "nudBerat"
+        nudBerat.Size = New Size(81, 23)
+        nudBerat.TabIndex = 3
+        ' 
+        ' nudTinggi
+        ' 
+        nudTinggi.DecimalPlaces = 2
+        nudTinggi.Location = New Point(198, 184)
+        nudTinggi.Maximum = New Decimal(New Integer() {999, 0, 0, 0})
+        nudTinggi.Name = "nudTinggi"
+        nudTinggi.Size = New Size(81, 23)
+        nudTinggi.TabIndex = 4
+        ' 
+        ' lblArahan
+        ' 
+        lblArahan.AutoSize = True
+        lblArahan.Location = New Point(362, 157)
+        lblArahan.Name = "lblArahan"
+        lblArahan.Size = New Size(198, 15)
+        lblArahan.TabIndex = 5
+        lblArahan.Text = "Sila masukkan berat dan tinggi anda"
+        ' 
+        ' BtnSubmit
+        ' 
+        BtnSubmit.Location = New Point(362, 112)
+        BtnSubmit.Name = "BtnSubmit"
+        BtnSubmit.Size = New Size(75, 23)
+        BtnSubmit.TabIndex = 6
+        BtnSubmit.Text = "Submit"
+        BtnSubmit.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(BtnSubmit)
+        Controls.Add(lblArahan)
+        Controls.Add(nudTinggi)
+        Controls.Add(nudBerat)
+        Controls.Add(Label2)
+        Controls.Add(lblBerat)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(nudBerat, ComponentModel.ISupportInitialize).EndInit()
+        CType(nudTinggi, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblBerat As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents nudBerat As NumericUpDown
+    Friend WithEvents nudTinggi As NumericUpDown
+    Friend WithEvents lblArahan As Label
+    Friend WithEvents BtnSubmit As Button
 
 End Class
--- bmitime/Form1.vb
+++ bmitime/Form1.vb
@@ -1,3 +1,26 @@
 ﻿Public Class Form1
 
+
+
+
+    Private Sub BtnSubmit_Click(sender As Object, e As EventArgs) Handles BtnSubmit.Click
+        Dim decBerat, decTinggi, decBmi As Decimal
+
+        decBerat = nudBerat.Value
+        decTinggi = nudTinggi.Value
+
+        decBmi = decBerat / (decTinggi ^ 2)
+
+        If decBmi < 18.5 Then
+            lblArahan.Text = "Bmi = Kurus" & Environment.NewLine & "Bmi = " & decBmi
+        ElseIf decBmi >= 18.5 AndAlso decBmi < 24.9 Then
+            lblArahan.Text = "Bmi = Normal" & Environment.NewLine & "Bmi = " & decBmi
+        ElseIf decBmi >= 25 AndAlso decBmi < 29.9 Then
+            lblArahan.Text = "Bmi = Gemuk" & Environment.NewLine & "Bmi = " & decBmi
+        Else
+            lblArahan.Text = "Bmi = Obesitas" & Environment.NewLine & "Bmi = " & decBmi
+        End If
+
+
+    End Sub
 End Class
